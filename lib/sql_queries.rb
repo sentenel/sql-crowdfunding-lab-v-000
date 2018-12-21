46e046e949b01e08c1b94167fd33ef072b9616d5
Seth Wax <sethlwax@gmail.com>
2018-12-21 01:28:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,10 @@ ORDER BY SUM(pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM pledges
+LEFT JOIN projects ON pledges.project_id = projects.id
+GROUP BY pledges.project_id
+HAVING projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
