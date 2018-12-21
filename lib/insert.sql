1f529047f03249604660c7f25056bd445e95f16c
Seth Wax <sethlwax@gmail.com>
2018-12-21 00:22:02 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL),
+(NIL, NIL, NIL, NIL, NIL);
