0ad29d080d029074a676b4fa8510c15f7af63a41
Seth Wax <sethlwax@gmail.com>
2018-12-21 00:36:27 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, SUM(pledges.amount) FROM projects
 LEFT JOIN pledges ON projects.id = pledges.project_id,
-GROUP BY projects.title
+GROUP BY pledges.project_id
 ORDER BY projects.title;"
 end
 
